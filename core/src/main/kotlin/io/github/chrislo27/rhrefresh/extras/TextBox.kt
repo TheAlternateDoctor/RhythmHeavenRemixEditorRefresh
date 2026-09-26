a9cf68864b1ca87409511e0dd1b9b7483a88be03
TheAlternateDoctor <thealternatedoc@gmail.com>
2026-09-26 13:00:01 +0200

    Cleaned some unused classes

--- core/src/main/kotlin/io/github/chrislo27/rhrefresh/extras/TextBox.kt
+++
@@ -1,5 +0,0 @@
-package io.github.chrislo27.rhrefresh.extras
-
-
-data class TextBox(val text: String, var requiresInput: Boolean, var secsBeforeCanInput: Float = 0.5f,
-                   var offsetX: Float = 0f, var offsetY: Float = 0f, var offsetW: Float = 0f, var offsetH: Float = 0f)
