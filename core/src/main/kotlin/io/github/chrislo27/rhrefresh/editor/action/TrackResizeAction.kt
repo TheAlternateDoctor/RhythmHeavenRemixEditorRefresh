a7d47aa2e3c732ebf371d54d89448b32911334c0
TheAlternateDoctor <thealternatedoc@gmail.com>
2026-09-26 12:34:43 +0200

    Fixed adding tracks to the bottom

--- core/src/main/kotlin/io/github/chrislo27/rhrefresh/editor/action/TrackResizeAction.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhrefresh/editor/action/TrackResizeAction.kt
@@ -2,6 +2,7 @@ package io.github.chrislo27.rhrefresh.editor.action
 
 import io.github.chrislo27.rhrefresh.PreferenceKeys.SETTINGS_NEW_TRACKS_ON_TOP
 import io.github.chrislo27.rhrefresh.editor.Editor
+import io.github.chrislo27.rhrefresh.entity.model.special.EndRemixEntity
 import io.github.chrislo27.rhrefresh.undoredo.ReversibleAction
 import io.github.chrislo27.rhrefresh.track.Remix
 
@@ -13,8 +14,10 @@ class TrackResizeAction(val editor: Editor, val oldSize: Int, val newSize: Int)
         context.trackCount = newSize
         if(!editor.main.preferences.getBoolean(SETTINGS_NEW_TRACKS_ON_TOP)){
             for(entity in editor.remix.entities){
-                entity.updateBounds {
-                    entity.bounds.setPosition(entity.bounds.x, entity.bounds.y + (newSize-oldSize))
+                if(entity !is EndRemixEntity){
+                    entity.updateBounds {
+                        entity.bounds.setPosition(entity.bounds.x, entity.bounds.y + (newSize-oldSize))
+                    }
                 }
             }
         }
@@ -22,6 +25,15 @@ class TrackResizeAction(val editor: Editor, val oldSize: Int, val newSize: Int)
 
     override fun undo(context: Remix) {
         context.trackCount = oldSize
+        if(!editor.main.preferences.getBoolean(SETTINGS_NEW_TRACKS_ON_TOP)){
+            for(entity in editor.remix.entities){
+                if(entity !is EndRemixEntity){
+                    entity.updateBounds {
+                        entity.bounds.setPosition(entity.bounds.x, entity.bounds.y + (oldSize-newSize))
+                    }
+                }
+            }
+        }
     }
 
 }
